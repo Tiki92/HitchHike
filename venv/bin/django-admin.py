a4dc2b574ea0d1d0bf03461f705ed02b2f61e585
Burca Vlad <tikiguy1642@gmail.com>
2019-07-04 19:33:04 +0300

    remove venv

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tiki/Documents/Projects/Django_Projects/HitchHike/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

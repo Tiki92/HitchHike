41a876472825321c4b1361c017d363f2fdb85cb2
Tiki92 <burca_vlad92@yahoo.com>
2019-07-04 11:26:01 +0300

    50% changed to class based views

--- HitchHike/FreeRides/urls.py
+++ HitchHike/FreeRides/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, re_path
-from . import views
+from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 
 urlpatterns = [
-path('', views.index, name='index'),
-path('accounts/profile/', views.profile_view, name='profile'),
-url(r'^accounts/profile/(?P<pk>\d+)/$', views.profile_view, name='profile_pk'),
-path('add_ride', views.add_ride, name='add_ride'),
-re_path(r'^(?P<ride_id>\d+)/$', views.detailed),
-path('accounts/profile/edit/', views.edit_profile, name='edit_profile'),
+path('', homeView.as_view(), name='index'),
+path('accounts/profile/', profile.as_view(), name='profile'),
+url(r'^accounts/profile/(?P<pk>\d+)/$', profile.as_view(), name='profile_pk'),
+path('add_ride', addRide.as_view(), name='add_ride'),
+re_path(r'^(?P<ride_id>\d+)/$', detailed.as_view()),
+path('accounts/profile/edit/', editProfile.as_view(), name='edit_profile'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- HitchHike/FreeRides/views.py
+++ HitchHike/FreeRides/views.py
@@ -1,90 +1,86 @@
-
+from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
-from .models import Rides
 from django.core.serializers import serialize
-import json
-from .forms import rideForm, EditProfileForm
 from django.http import HttpResponse
 from django.db.models import Q
 from django.contrib.gis.geoip2 import GeoIP2
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .forms import rideForm, EditProfileForm
+from .models import Rides
+import json
 
+class homeView(TemplateView):
+    template_name = 'Home.html'
 
-# Create your views here.
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
 
-def index(request):
-    loc = serialize('geojson', Rides.objects.all())
-    dec_loc = json.loads(loc)
+    def get(self, request):
+        loc = serialize('geojson', Rides.objects.all())
+        dec_loc = json.loads(loc)
 
-    obj = Rides.objects.all()
-    paginator = Paginator(obj, 12)
-    
-
-    query = request.GET.get('q', None)
-    if query is not None:
-        rides = obj.filter(
-            Q(location_name__icontains=query))
-        paginator = Paginator(rides, 12)
+        obj = Rides.objects.all()
+        paginator = Paginator(obj, 12)
         
-    page = request.GET.get('page')
-    rides = paginator.get_page(page)
 
-    g = GeoIP2()
-    ip = "185.86.151.11" # get_client_ip()
-    user_loc_lat = g.lat_lon(ip)[0]
-    user_loc_lon = g.lat_lon(ip)[1]
+        query = request.GET.get('q', None)
+        if query is not None:
+            rides = obj.filter(
+                Q(location_name__icontains=query))
+            paginator = Paginator(rides, 12)
+            
+        page = request.GET.get('page')
+        rides = paginator.get_page(page)
+
+        g = GeoIP2()
+        ip = "185.86.151.11" # self.get_client_ip(request)
+        user_loc_lat = g.lat_lon(ip)[0]
+        user_loc_lon = g.lat_lon(ip)[1]
+        
+        context = {'dec_loc':loc, 'rides': rides, 'user_loc_lat': user_loc_lat,
+         'user_loc_lon': user_loc_lon, 'obj': obj}
+        return render(request, 'Home.html', context)
+
+class profile(TemplateView):
+    template_name = 'account/Profile.html'
+    user = ''
+
     
-    context = {'dec_loc':loc, 'rides': rides, 'user_loc_lat': user_loc_lat, 'user_loc_lon': user_loc_lon, 'obj': obj}
-    return render(request, 'Home.html', context)
+    def get(self, request, pk=None):
+        user = 0
+        if pk:
+            user = User.objects.get(pk=pk)
+        else:
+            user = User.objects.get(username=request.user)
+        args = {'user':user}
+        return render(request, 'account/Profile.html', args)
 
-@login_required
-def profile_view(request, pk=None):
-    if pk:
-        user = User.objects.get(pk=pk)
-    else:
-        user = User.objects.get(username=request.user)
-    args = {'user':user}
-    return render(request, 'account/Profile.html', args)
 
-@login_required
-def add_ride(request):
-    if request.method == "POST":
+class addRide(TemplateView):
+    template_name = 'add_ride.html'
+
+    def post(self, request):
         form =rideForm(request.POST)
         if form.is_valid():
             form.cleaned_data
             post = form.save(commit=False)
             form.save()
-            return HttpResponse("FUCKK!!YOUUU!!")
+            return redirect('FreeRides:profile')
 
-        return HttpResponse(form.errors)
-
-    return render(request, 'add_ride.html')
+        #return HttpResponse(form.errors)
 
 
-def detailed(request,ride_id):
-    details = Rides.objects.all()
-    ride = Rides.objects.get(id=ride_id)
-    context = {'details': details, 'ride': ride}
-    return render(request, "detailed.html", context)
+
+class detailed(TemplateView):
+    template_name = "detailed.html"
 
 
-def edit_profile(request):
-    if request.method == 'POST':
-        form = EditProfileForm(request.POST, instance=request.user)
-
-        if form.is_valid():
-            form.save()
-            return redirect('/accounts/profile/')
-    else:
-        form = EditProfileForm(instance=request.user)
-        args = {'form': form}
-        return render(request, 'account/edit_profile.html', args)
+class editProfile(TemplateView):
+    template_name = 'account/edit_profile.html'
